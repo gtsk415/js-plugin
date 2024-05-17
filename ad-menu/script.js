99e6c28ff7c6e66ed9e5a1400e4939f2b48d6d4d
gtsk415 <print.bistro@mail.ru>
2024-05-17 16:26:27 +0300

    done v1

--- ad-menu/script.js
+++ ad-menu/script.js
@@ -10,92 +10,91 @@ document.addEventListener('DOMContentLoaded', () => {
 		return nextElements;
 	}
 
-  const menu = document.querySelector('.menu-main');
-  const more = document.querySelector('.menu-more');
-  const subMenu = document.querySelector('.menu-sub');
-  const parent = document.querySelector('.menu-wrapper');
-  const root = document.documentElement;
-  const moreWidth = getComputedStyle(root).getPropertyValue('--more-width');
-  const moreMargin = getComputedStyle(root).getPropertyValue('--more-margin');
-  const menuMargin = getComputedStyle(root).getPropertyValue('--menu-margin');
+	const menu = document.querySelector('.menu-main'),
+				more = document.querySelector('.menu-more'),
+				subMenu = document.querySelector('.menu-sub'),
+				parent = document.querySelector('.menu-wrapper'),
+				root = document.documentElement,
+				moreWidth = getComputedStyle(root).getPropertyValue('--more-width'),
+				moreMargin = getComputedStyle(root).getPropertyValue('--more-margin'),
+				menuMargin = getComputedStyle(root).getPropertyValue('--menu-margin');
+	
+	let windowWidth = window.innerWidth;
 
-  let windowWidth = window.innerWidth;
+	const contract = () => {
+		let w = 0,
+				outerWidth = parent.offsetWidth - (parseInt(moreWidth) + parseInt(moreMargin) + parseInt(menuMargin) + 30);
 
-  const contract = () => {
-    let w = 0;
-    let outerWidth = parent.offsetWidth - (parseInt(moreWidth) + parseInt(moreMargin) + parseInt(menuMargin) + 30);
+		let menuItems = menu.querySelectorAll('li');
 
-    let menuItems = menu.querySelectorAll('li');
+		for (let i = 0; i < menuItems.length; i++) {
+			w += menuItems[i].offsetWidth;
 
-    for (let i = 0; i < menuItems.length; i++) {
-      w += menuItems[i].offsetWidth;
+			if (w > outerWidth) {
+				let nextElements = nextAll(menuItems[i - 1]);
 
-      if (w > outerWidth) {
-        let nextElements = nextAll(menuItems[i - 1]);
+				let nextReverse = nextElements.reverse();
 
-        let nextReverse = nextElements.reverse();
+				nextReverse.forEach(el => {
+					el.remove();
+					subMenu.prepend(el);
+				});
 
-        nextReverse.forEach(el => {
-          el.remove();
-          subMenu.prepend(el);
-        });
+				break;
+			}
+		}
+	};
 
-        break;
+	const expand = () => {
+		let w = 0,
+				outerWidth = parent.offsetWidth - (parseInt(moreWidth) + parseInt(moreMargin) + parseInt(menuMargin) + 30);
 
-      }
-    }
-  };
+		let menuItems = menu.querySelectorAll('li');
+		menuItems.forEach(el => {
+			w += el.offsetWidth;
+		});
 
-  const expand = () => {
-    let w = 0;
-    let outerWidth = parent.offsetWidth - (parseInt(moreWidth)) + (parseInt(moreMargin)) + (parseInt(menuMargin));
+		let submenuItems = subMenu.querySelectorAll('li');
+		for (let i = 0; i < submenuItems.length; i++) {
+			w += submenuItems[i].offsetWidth;
+			if (w > outerWidth) {
+				let a = 0;
 
-    let menuItems = menu.querySelectorAll('li');
-    menuItems.forEach( el => {
-      w += el.offsetWidth;
-    });
+				while(a < i) {
+					submenuItems[a].remove();
+					menu.appendChild(submenuItems[a]);
 
-    let submenuItems = subMenu.querySelectorAll('li');
-    for (let i = 0; i < submenuItems.length; i++) {
-      w += submenuItems[i].offsetWidth;
-      if (w > outerWidth) {
-        let a = 0;
+					a++;
+				}
 
-        while (a < i) {
-          submenuItems[i].remove();
-          menu.appendChild(submenuItems[a]);
+				break;
+			}
+		}
 
-          a++
-        }
-        break;
-      }
-    }
+		if (submenuItems.length > 0) {
+			let lastOffset = submenuItems[submenuItems.length - 1].offsetWidth;
 
-    if (submenuItems.length > 0) {
-      let lastOffset = submenuItems[submenuItems.length - 1].offsetWidth;
+			if ((menu.offsetWidth + lastOffset) <= outerWidth) {
+				submenuItems[submenuItems.length - 1].remove();
+				menu.appendChild(submenuItems[submenuItems.length - 1]);
+			}
+		}
+	};
 
-      if ((menu.offsetWidth + lastOffset) <= outerWidth) {
-        submenuItems[submenuItems.length - 1].remove();
-        menu.appendChild(submenuItems[submenuItems.length -1]);
-      }
-    }
-  };
+	const checkActive = () => {
+		if (subMenu.querySelectorAll('li').length) {
+			more.classList.add('active');
+		} else {
+			more.classList.remove('active');
+		}
+	};
 
-  const checkActive = () => {
-    if (subMenu.querySelectorAll('li').length) {
-      more.classList.add('active');
-    } else {
-      more.classList.remove('active');
-    }
-  };
-
-  contract();
-  checkActive();
-
-  window.addEventListener('resize', () => {
-    (window.innerWidth > windowWidth) ? expand() : contract();
-    windowWidth = window.innerWidth;
-    checkActive();
-  });
-
+	contract();
+	checkActive();
+	
+	window.addEventListener('resize', () => {
+		(window.innerWidth > windowWidth) ? expand() : contract();
+		windowWidth = window.innerWidth;
+		checkActive();
+	});
 });
